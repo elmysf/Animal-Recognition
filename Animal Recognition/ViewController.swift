bda1534add29ba102d7485eb0bb4a79ae722ba3e
Sufiandy Elmy <elmysufiandy09@gmail.com>
2020-05-18 16:20:06 +0700

    Initial Commit

---
+++ Animal Recognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animal Recognition
+//
+//  Created by Sufiandy Elmy on 18/05/20.
+//  Copyright © 2020 Sufiandy Elmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
